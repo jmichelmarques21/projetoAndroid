42f13f7ff3446889d54c1906a3ea23e28cd9fb5a
jmichelmarques21 <jmichelmarques21@gmail.com>
2024-09-02 23:50:08 -0300

    projeto android

---
+++ app/src/main/java/com/example/jornadadaconquista/MainActivity.kt
@@ -0,0 +1,117 @@
+package com.example.jornadadaconquista
+
+import android.graphics.Paint.Align
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+// import androidx.compose.material.Button
+// import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jornadadaconquista.ui.theme.JornadaDaConquistaTheme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity(){
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JornadaDaConquistaTheme {
+                JornadaDaConquista()
+            }
+        }
+    }
+}
+
+private fun ColumnScope.Button(onClick: @Composable () -> Unit) {
+
+}
+
+fun Button(onClick: () -> Unit, function: @Composable () -> Unit) {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun JornadaDaConquista(){
+    var cliques by remember {mutableStateOf(0) }
+    var estadoJogo by remember { mutableStateOf(JogoEstado.INICIAL) }
+    val N = remember { Random.nextInt(1,51) } // gera um número aleatório entre 1 e 50
+    val imagem = when {
+        cliques == N ->R.drawable.conquista
+        cliques >= (N * 0.66).toInt() -> R.drawable.imgfinal
+        cliques >= (N * 0.33).toInt() -> R.drawable.mediana
+        else -> R.drawable.inicial
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = imagem), contentDescription = null, modifier = Modifier
+            .size(200.dp)
+            .clickable {
+                if (cliques < N) {
+                    cliques++
+                    if (cliques == N) {
+                        estadoJogo = JogoEstado.CONQUISTA
+                    }
+                }
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            estadoJogo = JogoEstado.DESISTENCIA
+        }) {
+            Text("Desistir")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        if(estadoJogo == JogoEstado.CONQUISTA) {
+            Text("Parabéns! Você alcançou a conquista!")
+            Button(onClick =  {reiniciarJogo()}) {
+                Text("Novo Jogo")
+            }
+        } else if(estadoJogo == JogoEstado.DESISTENCIA) {
+            Image(
+                painter = painterResource(id = R.drawable.desistencia),
+                contentDescription = null,
+                modifier = Modifier.size(200.dp)
+            )
+            Text("Você desistiu. Deseja tentar novamente? ")
+            Row {
+                Button(onClick =  {reiniciarJogo() }) {
+                    Text("Sim")
+                }
+                this@Column.Button(onClick = {
+                    Text("Não")
+                    }
+                )}
+        }
+    }
+}
+
+
+
+
+fun reiniciarJogo() {
+        var cliques = 0;
+        var estadoJogo = JogoEstado.INICIAL
+    }
+enum class JogoEstado {
+    INICIAL, MEDIANA, FINAL, CONQUISTA, DESISTENCIA
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview(){
+    JornadaDaConquistaTheme {
+        JornadaDaConquista()
+    }
+}
